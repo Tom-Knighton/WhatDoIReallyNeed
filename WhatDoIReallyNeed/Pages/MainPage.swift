289753af0dcd7f0bef6a6435aa7fb1b9bbc4eaa0
Tom K <tomknighton@icloud.com>
2022-09-11 11:38:52 +0100

    tweak(Main): embed home list in scrollview

--- WhatDoIReallyNeed/Pages/MainPage.swift
+++ WhatDoIReallyNeed/Pages/MainPage.swift
@@ -31,44 +31,47 @@ struct HomePage: View {
                 .multilineTextAlignment(.leading)
                 .font(.callout.bold())
             
-            LazyVGrid(columns: layoutGrid) {
-                ForEach(homes) { home in
-                    layout {
-                        Circle()
-                            .fill(Color(hex: home.homeColour))
-                            .frame(width: 50, height: 50)
-                            .shadow(radius: 1)
-                            .overlay(
-                                Group {
-                                    if home.homeIcon.starts(with: "SFS_") {
-                                        Image(systemName: home.homeIcon.replacingOccurrences(of: "SFS_", with: ""))
-                                    } else {
-                                        Text(home.homeIcon)
+            ScrollView {
+                LazyVGrid(columns: layoutGrid) {
+                    ForEach(homes) { home in
+                        layout {
+                            Circle()
+                                .fill(Color(hex: home.homeColour))
+                                .frame(width: 50, height: 50)
+                                .shadow(radius: 1)
+                                .overlay(
+                                    Group {
+                                        if home.homeIcon.starts(with: "SFS_") {
+                                            Image(systemName: home.homeIcon.replacingOccurrences(of: "SFS_", with: ""))
+                                        } else {
+                                            Text(home.homeIcon)
+                                        }
                                     }
-                                }
-                                .font(.system(size: 25))
-                                .foregroundColor(.white)
-                                
-                            )
-                        Text(home.homeName)
-                            .font(.title3.bold())
-                            .minimumScaleFactor(0.1)
-                        
-                        if UIDevice.current.userInterfaceIdiom == .pad {
-                            Spacer()
+                                    .font(.system(size: 25))
+                                    .foregroundColor(.white)
+                                    
+                                )
+                            Text(home.homeName)
+                                .font(.title3.bold())
+                                .minimumScaleFactor(0.1)
+                            
+                            if UIDevice.current.userInterfaceIdiom == .pad {
+                                Spacer()
+                            }
                         }
+                        .frame(maxWidth: .infinity, minHeight: 50, maxHeight: 100)
+                        .padding()
+                        .background(
+                            Color(hex: home.homeColour)
+                                .overlay(Material.thin)
+                        )
+                        .cornerRadius(10)
+                        .shadow(color: Color(hex: home.homeColour ), radius: 1)
+                        .padding(.vertical, 8)
                     }
-                    .frame(maxWidth: .infinity, minHeight: 50, maxHeight: 100)
-                    .padding()
-                    .background(
-                        Color(hex: home.homeColour)
-                            .overlay(Material.thin)
-                    )
-                    .cornerRadius(10)
-                    .shadow(color: Color(hex: home.homeColour ), radius: 1)
-                    .padding(.vertical, 8)
                 }
             }
+            
 
             Spacer()
         }
